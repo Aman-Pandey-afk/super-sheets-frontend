183aba16deab9de65d18f40229bb1cbfacc54de4
aman-pandey-afk <aman_p@ph.iitr.ac.in>
2022-03-13 00:46:53 +0530

    feat: Add Table

--- src/ui/views/ProjectScreen/index.tsx
+++ src/ui/views/ProjectScreen/index.tsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Field } from "../../../models/Field";
 
-const ProjectScreen: React.FC = () => {
-  return (
-    <div className="project-container">
-      <th>
-        <td className="header-row">name</td>
-        <td className="header-row">enrollment no</td>
-        <td className="header-row">github id</td>
-      </th>
+export default function ProjectScreen() {
+  const[datas, SetData] = useState<any[]>([]);
+  const[fields, SetFields] = useState<any[]>([]);
+
+  const GetData = async() => {
+    const response = await fetch(
+      "https://f66b-103-37-201-178.ngrok.io/table/soc"
+    );
+    const data = await response.json();
+    console.log(data)
+    SetData(data);
+  };
+
+  const GetFields = async() => {
+    const response = await fetch(
+      "https://f66b-103-37-201-178.ngrok.io/fields/soc"
+    );
+    const data = await response.json();
+    SetFields(data);
+  };
+
+  useEffect(() => {
+    GetData();
+    GetFields();
+  }, []);
+
+  const fieldArr:string[] = [];
+
+  const TableHead = fields.map((info)=>{
+    fieldArr.push(info.fieldSlug);
+    return(
+      <td>
+          <th>{info.displayName}</th>
+      </td>
+  )});
+
+  const DisplayData = datas.map((info)=>{
+    return(
       <tr>
-        <td className="data-row">Anand Kumar</td>
-        <td className="data-row">20114009</td>
-        <td className="data-row">anand817</td>
+      {fieldArr.map((el)=><td>{info[el]}</td>)}
       </tr>
+  )});
+
+  return (
+    <div className="ProjectScreen">
+       <div>
+          <table className="table table-striped">
+              <thead>
+                  {TableHead}
+              </thead>
+              <tbody>    
+                  {DisplayData}                  
+              </tbody>
+          </table>
+           
+      </div>
     </div>
   );
-};
-
-export default ProjectScreen;
+}
